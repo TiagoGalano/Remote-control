ee55dd9fac822821d75aa71b53503513c4a75c23
Tiago Batista Montenegro Galano <167694783+TiagoGalano@users.noreply.github.com>
2025-03-24 21:37:13 +0000

    Add files via upload

---
+++ Controle/Led.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt;
+using System.Windows.Threading;
+
+namespace Controle
+{
+    class Led
+    {
+        private MqttClient client;
+        private string clientId;
+        private RichTextBox rtxtOut;
+        public Led(RichTextBox rtxt)
+        {
+            rtxtOut = rtxt;
+            var x = rtxtOut.Handle;
+            string BrokerAddress = "89.154.162.243";
+            string user = "profmiguel";
+            string pass = "Esas2024";
+
+            client = new MqttClient(BrokerAddress);
+            
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId, user, pass);
+            
+            string[] topicos = new string[] { "led1", "led2", "led3", "led4" };
+
+            
+            byte[] qosLevels = new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE,
+                                            MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE,
+                                            MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE,
+                                            MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE };
+
+            client.Subscribe(topicos, qosLevels);            
+        }
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // handle message received
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+            rtxtOut.Invoke((MethodInvoker)(() => rtxtOut.Text += "\n" + ReceivedMessage));
+        }
+        public bool publishLigar(string num)
+        {
+            try
+            {
+                switch (num)
+                {
+                    case "1":
+                        client.Publish("led1", Encoding.UTF8.GetBytes("ON"));
+                        break;
+                    case "2":
+                        client.Publish("led2", Encoding.UTF8.GetBytes("ON"));
+                        break;
+                    case "3":
+                        client.Publish("led3", Encoding.UTF8.GetBytes("ON"));
+                        break;
+                    case "4":
+                        client.Publish("led4", Encoding.UTF8.GetBytes("ON"));
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool publishDesligar(string num)
+        {
+            try
+            {
+                switch (num)
+                {
+                    case "1":
+                        client.Publish("led1", Encoding.UTF8.GetBytes("OFF"));
+                        break;
+                    case "2":
+                        client.Publish("led2", Encoding.UTF8.GetBytes("OFF"));
+                        break;
+                    case "3":
+                        client.Publish("led3", Encoding.UTF8.GetBytes("OFF"));
+                        break;
+                    case "4":
+                        client.Publish("led4", Encoding.UTF8.GetBytes("OFF"));
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Controle/Webcam.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt;
+using System.Windows.Threading;
+
+namespace Controle
+{
+    class Webcam
+    {
+        private MqttClient client;
+        private string clientId;
+        private RichTextBox rtxtOut;
+        public Webcam(RichTextBox rtxt)
+        {
+            rtxtOut = rtxt;
+            var x = rtxtOut.Handle;
+            string BrokerAddress = "89.154.162.243";
+            string user = "profmiguel";
+            string pass = "Esas2024";
+
+            client = new MqttClient(BrokerAddress);
+
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId, user, pass);
+
+            client.Subscribe(new string[] { "Webcam" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+        }
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // handle message received
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+            rtxtOut.Invoke((MethodInvoker)(() => rtxtOut.Text += "\n" + ReceivedMessage));
+        }
+        public bool publishEsq()
+        {
+            try
+            {
+                client.Publish("Webcam", Encoding.UTF8.GetBytes("ESQ"));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool publishDir()
+        {
+            try
+            {
+                client.Publish("Webcam", Encoding.UTF8.GetBytes("DIR"));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool publishCima()
+        {
+            try
+            {
+                client.Publish("Webcam", Encoding.UTF8.GetBytes("CIM"));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool publishBaixo()
+        {
+            try
+            {
+                client.Publish("Webcam", Encoding.UTF8.GetBytes("BAI"));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
